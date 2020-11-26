486a825a711140c69342538c695aa6de74b4bb18
PolisGuy <31580704+PolisGuy@users.noreply.github.com>
2020-11-25 22:59:56 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 systemctl stop Polis
 echo "Downloading Polis binaries"
+rm polisd
+rm polis-cli
 wget https://github.com/PolisGuy/scripts/raw/main/bin/polisd
 wget https://github.com/PolisGuy/scripts/raw/main/bin/polis-cli
 mv polisd /usr/local/bin/
