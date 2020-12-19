1ce0c0484b965bf56efae3e129e86370b31187b6
PolisGuy <31580704+PolisGuy@users.noreply.github.com>
2020-12-19 14:38:34 -0500

    Update resetchain.sh

--- resetchain.sh
+++ resetchain.sh
@@ -16,23 +16,24 @@ systemctl stop Polis
 sleep 15
 echo
 echo "Erasing chain data"
-rm /root/.poliscore/.lock
-rm /root/.poliscore/banlist.dat
 rm -R /root/.poliscore/blocks/
 rm -R /root/.poliscore/chainstate/
-rm /root/.poliscore/db.log
-rm /root/.poliscore/debug.log
 rm -R /root/.poliscore/evodb/
-rm /root/.poliscore/fee_estimates.dat
-rm /root/.poliscore/governance.dat
-rm /root/.poliscore/instantsend.dat
 rm -R /root/.poliscore/llmq/
-rm /root/.poliscore/mempool.dat
-rm /root/.poliscore/mncache.dat
-rm /root/.poliscore/netfulfilled.dat
-rm /root/.poliscore/peers.dat
 rm /root/.poliscore/sporks.dat
 rm /root/bootstrap*
+#below are optional if you really want to clean things up
+#rm /root/.poliscore/.lock
+#rm /root/.poliscore/banlist.dat
+#rm /root/.poliscore/db.log
+#rm /root/.poliscore/debug.log
+#rm /root/.poliscore/fee_estimates.dat
+#rm /root/.poliscore/governance.dat
+#rm /root/.poliscore/instantsend.dat
+#rm /root/.poliscore/mempool.dat
+#rm /root/.poliscore/mncache.dat
+#rm /root/.poliscore/netfulfilled.dat
+#rm /root/.poliscore/peers.dat
 echo
 echo "Downloading bootstrap"
 wget http://159.203.48.172/bootstrap.7z
