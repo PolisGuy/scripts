4a7aa95ead662adc2cd5b7a4672024d78efb22da
PolisGuy <31580704+PolisGuy@users.noreply.github.com>
2020-12-19 19:56:04 -0500

    Update resetchain.ps1

--- powershell/resetchain.ps1
+++ powershell/resetchain.ps1
@@ -10,9 +10,9 @@ Write-Output "7. Start Polis."
 read-host "Press ENTER to continue..."
 $ProgressPreference = 'SilentlyContinue'
 #the bootstrap gets remade at the top of every hour and takes 5 minutes so wait until is is 5 past the hour to start
-while ((Get-Date).Minute -lt 5)
+while ((Invoke-WebRequest -Uri http://159.203.48.172/rebuilding.txt -UseBasicParsing).Content -eq "true")
 {
-  echo "The bootstrap file is being updated on the server. Waiting until 5 minutes after the hour. Pausing for 15 seconds."
+  echo "The bootstrap file is being updated on the server. Checking again in 15 seconds."
   sleep 15
 }
 
@@ -95,4 +95,4 @@ else
     & 'C:\Program Files (x86)\PolisCore\polis-qt.exe'
 }
 Write-Output "`nScript complete. Polis will complete syncing shortly. Thank you for being a member of the Polis community!"
-pause
+read-host "Press ENTER to exit."
